93f484a5b2ca4d210d31c56c0cc9c14fcd56d986
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-05-07 18:41:31 -0300

    Creando scripts para compilar en travis.

---
+++ crear_binarios.sh
@@ -0,0 +1,3 @@
+ember electron:package --platform=win32
+zip -qr visor-offline-windows-32bits.zip electron-builds/visor-offline-win32-ia32/
+zip -qr visor-offline-windows-64bits.zip electron-builds/visor-offline-win32-x64/
